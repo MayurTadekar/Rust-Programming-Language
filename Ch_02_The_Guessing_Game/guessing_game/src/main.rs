75601e1ef272c4451616ba27e4d7588179d08b94
MayurTadekar <mrtadekar@gmail.com>
2024-08-28 22:51:44 +0530

    10:51 PM - 28/08/2024 - Save Dates

--- Ch_02_The_Guessing_Game/guessing_game/src/main.rs
+++ Ch_02_The_Guessing_Game/guessing_game/src/main.rs
@@ -18,24 +18,30 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Pease Type a number!"); 
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Pease Type a number!"); 
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Win!!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!!!");
+                break;            
+            }
+        }
     }
 } 
